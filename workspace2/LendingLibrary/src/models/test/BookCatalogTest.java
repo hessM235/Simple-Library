e419c5963b0fddfaf5cf9c5017d0ce233d7751a1
Michael Hess <m.hess235@gmail.com>
2015-05-04 23:46:21 -0400

    Added findABookThatDoesNot Exist test to BookCatalog Test

--- workspace2/LendingLibrary/src/models/test/BookCatalogTest.java
+++ workspace2/LendingLibrary/src/models/test/BookCatalogTest.java
@@ -3,6 +3,7 @@ package models.test;
 import static org.junit.Assert.*;
 import models.Book;
 import models.BookCatalog;
+import models.BookNotFoundException;
 
 import org.junit.Test;
 
@@ -16,8 +17,36 @@ public class BookCatalogTest {
 
 		Book book = new Book(1, "", "", "", "", 0);
 		bc.addBook(book);
+
+		assertTrue(initialNumber == bc.getNumberOfBooks() - 1);
+	}
+
+	@Test
+	public void testFindABook() {
+
+		BookCatalog bc = new BookCatalog();
+		int initialNumber = bc.getNumberOfBooks();
+
+		Book book = new Book(1, "Intro To Java", "", "", "", 0);
+		bc.addBook(book);
+
+		try {
+			Book foundBook = bc.findBook("Intro To Java");
+		} catch (BookNotFoundException e) {
+			fail("Book Not Found");
+		}
+		assertTrue(initialNumber == bc.getNumberOfBooks() - 1);
+	}
+
+	@Test(expected = BookNotFoundException.class)
+	public void testFindABookThatDoesNotExist() throws BookNotFoundException {
+
+		BookCatalog bc = new BookCatalog();
 		
-		assertTrue(initialNumber == bc.getNumberOfBooks() -1);
+		Book book = new Book(1, "Intro To Java", "", "", "", 0);
+		bc.addBook(book);
+
+		Book foundBook = bc.findBook("Thinking In Java");
 	}
 
 }
