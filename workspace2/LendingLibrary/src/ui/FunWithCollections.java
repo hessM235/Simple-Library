03bcd0f0564b4e4feb75cc8e4a4c1361fb297614
Michael Hess <m.hess235@gmail.com>
2015-05-11 01:48:29 -0400

    5/11/2015

--- workspace2/LendingLibrary/src/ui/FunWithCollections.java
+++ workspace2/LendingLibrary/src/ui/FunWithCollections.java
@@ -8,18 +8,18 @@ public class FunWithCollections {
 		
 		ArrayList<String> myArrayList = new ArrayList<String>();
 		
-		myArrayList.add("first item");
-		boolean result = myArrayList.add("second item");
+		myArrayList.add("First item");
+		boolean result = myArrayList.add("Second item");
 		
 		System.out.println(result);
-		myArrayList.add("third item");
-		myArrayList.add("fourth item");
+		myArrayList.add("Third item");
+		myArrayList.add("Fourth item");
 		
 		System.out.println(myArrayList.size());
 		
 		myArrayList.remove(1);
-		myArrayList.add("fifth item");
-		myArrayList.add(1, "sixth item");
+		myArrayList.add("Fifth item");
+		myArrayList.add(1, "Sixth item");
 		
 		for(int i =0 ; i < myArrayList.size(); i++) {
 			System.out.println(myArrayList.get(i));
